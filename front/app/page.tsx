6b15c208d3f685e46871bdcdc9c52de4b6b18071
Kawa_ <yuyux0910@gmail.com>
2025-04-22 00:42:17 +0000

    feature: 要素をグリッドを利用して表示#1

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,5 +1,41 @@
+import '@/app/globals.css'
+
 export default function Home() {
+  const arr = [
+    {name: 'a',},
+    {name: 'b',},
+    {name: 'c',},
+    {name: 'd',},
+    {name: 'e',},
+    {name: 'f',},
+    {name: 'g',},
+    {name: 'h',},
+    {name: 'i',},
+    {name: 'j',},
+  ]
+
   return (
-    <div>Hello World!</div>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div
+        className={`grid grid-cols-5 gap-0`}
+        style={{ width: 'fit-content' }}
+      >
+        {arr.map((value: {name: string;}, index: number)=>
+          <div
+            key={`${value}_${index}`}
+            className="w-16 h-16 bg-white border border-gray-300 hover:bg-gray-200"
+          >
+            value
+          </div>
+
+        )}
+        {/* {Array.from({ length: size * size }, (_, i) => (
+          <div
+            key={`test_${i}`}
+            className="w-16 h-16 bg-white border border-gray-300 hover:bg-gray-200"
+          />
+        ))} */}
+      </div>
+    </div>
   );
 }
