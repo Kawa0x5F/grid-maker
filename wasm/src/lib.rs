d53ffffd619311e04c8f93ae0eec8ba119460973
Kawa_ <yuyux0910@gmail.com>
2025-04-30 10:56:33 +0000

    feature: size分の配列を返す #10

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -12,3 +12,12 @@ extern "C" {
 pub fn alert_input_size(size: usize) {
     alert(&format!("Input Size is {}", size));
 }
+
+#[wasm_bindgen]
+pub fn get_array(size: usize) -> Vec<i32> {
+    let mut array = Vec::with_capacity(size * size);
+    for _ in 0..size {
+        array.extend(vec![0i32; size]);
+    }
+    array
+}
