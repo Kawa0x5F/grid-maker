9c2d4c1c25255d03f04ff2467446e8f471db1e9d
Kawa_ <yuyux0910@gmail.com>
2025-04-30 10:12:58 +0000

    feature: 変数sizeを入力するフォームを追加 #10

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { useEffect } from "react";
+import { useState } from "react";
 import { alert_input_size } from '@/app/src/wasm/rust_wasm'
 import '@/app/globals.css'
 
 export default function Home() {
-  // const [size, setSize] = useState("");
+  const [size, setSize] = useState(0);
   const arr = [
     {name: 'a',},
     {name: 'b',},
@@ -19,22 +19,57 @@ export default function Home() {
     {name: 'j',},
   ]
 
-  useEffect(() => {
-      alert_input_size(5);
-  }, []);
+  const handleSizeKeyDown = (e) => {
+    if (e.key == 'Enter') { // エンターキーを押されたとき
+      alert_input_size(size);
+    }
+  }
+
+  const handleSizeSubmitButton = (e) => {
+    alert_input_size(size);
+  }
+
+  const handleChangeSize = (e) => {
+    const input = e.target.value;
+    if (/^\d*$/.test(input)) {
+      setSize(input);
+    }
+  }
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div
-        className={`grid grid-cols-5 gap-0`}
-        style={{ width: 'fit-content' }}
+    <div className="flex flex-col justify-center items-center bg-gray-100">
+      <form 
+        onSubmit={handleSizeSubmitButton}
+        className="flex flex-col items-center justify-center space-y-3 w-full"
       >
-        {arr.map((value: {name: string;}, index: number)=>
-          <div
-            key={`${value}_${index}`}
-            className="w-16 h-16 bg-white border border-gray-300 hover:bg-gray-200"
-           />
-        )}
+        <input
+          type="text"
+          id="only-number"
+          inputMode="numeric"
+          onChange={handleChangeSize}
+          onKeyDown={handleSizeKeyDown}
+          className="border rounded px-4 py-2 w-48 text-center shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
+        />
+        <button
+          type="submit"
+          className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded shadow-md"
+        >
+          決定
+        </button>
+      </form>
+
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <div
+          className={`grid grid-cols-5 gap-0`}
+          style={{ width: 'fit-content' }}
+        >
+          {arr.map((value: {name: string;}, index: number)=>
+            <div
+              key={`${value}_${index}`}
+              className="w-16 h-16 bg-white border border-gray-300 hover:bg-gray-200"
+            />
+          )}
+        </div>
       </div>
     </div>
   );
