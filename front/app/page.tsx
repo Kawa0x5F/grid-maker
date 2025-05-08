04c25fc24de7861a45e4746e4a0a9f7532e243bd
Kawa_ <yuyux0910@gmail.com>
2025-05-08 05:11:27 +0000

    feature: 行列の数値からグリッドを表示 #14

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -5,27 +5,30 @@ import { get_array } from '@/app/src/wasm/rust_wasm'
 import '@/app/globals.css'
 
 export default function Home() {
-  const [size, setSize] = useState(0);
+  const [size, setSize] = useState({
+    row: 5,
+    col: 5,
+  });
   const [array, setArray] = useState<number[]>([]);
 
   const handleSizeKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key == 'Enter') { // エンターキーを押されたとき
       e.preventDefault()
-      const res = get_array(size);
+      const res = get_array(size.row, size.col);
       setArray(Array.from(res));
     }
   }
 
   const handleSizeSubmitButton = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const res = get_array(size);
+    const res = get_array(size.row, size.col);
     setArray(Array.from(res));
 }
 
   const handleChangeSize = (e: React.ChangeEvent<HTMLInputElement>) => {
     const input = e.target.value;
     if (/^\d*$/.test(input)) { // 文字列が数字のみで構成されているかを見る
-      setSize(Number(input)); // inputは文字列なので数値に変換してset
+      setSize((prev) => ({ ...prev, [e.target.name]: e.target.value }));
     }
   }
 
@@ -36,6 +39,7 @@ export default function Home() {
         className="flex flex-col items-center justify-center space-y-3 w-full"
       >
         <input
+          name="row"
           type="text"
           id="only-number"
           inputMode="numeric"
@@ -43,7 +47,15 @@ export default function Home() {
           onKeyDown={handleSizeKeyDown}
           className="border rounded px-4 py-2 w-48 text-center shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
         />
-        <button
+        <input
+          name="col"
+          type="text"
+          id="only-number"
+          inputMode="numeric"
+          onChange={handleChangeSize}
+          onKeyDown={handleSizeKeyDown}
+          className="border rounded px-4 py-2 w-48 text-center shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
+        />        <button
           type="submit"
           className="px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded shadow-md"
         >
@@ -54,7 +66,7 @@ export default function Home() {
       <div className="flex justify-center items-center min-h-screen bg-gray-100">
         <div
           className={`grid grid-cols-[repeat(var(--cols),minmax(0,1fr))] gap-0`}
-          style={{ width: 'fit-content', '--cols': size } as React.CSSProperties}
+          style={{ width: 'fit-content', '--cols': size.row } as React.CSSProperties}
         >
           {array.map((value: number, index: number)=>
             <div
