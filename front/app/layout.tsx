b877d3df3b730015d1a3f48beb7f4be8649a4ca9
Kawa_ <yuyux0910@gmail.com>
2025-08-14 08:44:37 +0000

    fix: エラーを修正#14

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -4,13 +4,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html>
+    <html  lang='en' suppressHydrationWarning>
       <link rel="icon" type="image/png" href="/favicons/favicon-96x96.png" sizes="96x96" />
       <link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
       <link rel="shortcut icon" href="/favicons/favicon.ico" />
       <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
       <link rel="manifest" href="/favicons/site.webmanifest" />
-      <body>{children}</body>
+      <body suppressHydrationWarning>{children}</body>
     </html>
   );
 }
